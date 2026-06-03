0579b3bee254886d9fcc3719575b661d66df868e
Oliver <oliver139@berkeley.edu>
2026-06-03 00:06:02 -0700

    Add 25 Thai recipes (123 total)

--- src/data/globalRecipes/index.ts
+++ src/data/globalRecipes/index.ts
@@ -2,10 +2,12 @@ import { EAST_ASIA_RECIPES } from "./eastAsia";
 import { JAPANESE_RECIPES } from "./japanese";
 import { KOREAN_RECIPES } from "./korean";
 import { TAIWANESE_RECIPES } from "./taiwanese";
+import { THAI_RECIPES } from "./thai";
 
 export const GLOBAL_RECIPES = [
   ...EAST_ASIA_RECIPES,
   ...JAPANESE_RECIPES,
   ...KOREAN_RECIPES,
   ...TAIWANESE_RECIPES,
+  ...THAI_RECIPES,
 ];
