4ba52918793d550997294438969fadf61b0167ea
Oliver <oliver139@berkeley.edu>
2026-06-02 23:54:34 -0700

    Add 20 Taiwanese recipes (98 total)

--- src/data/globalRecipes/index.ts
+++ src/data/globalRecipes/index.ts
@@ -1,9 +1,11 @@
 import { EAST_ASIA_RECIPES } from "./eastAsia";
 import { JAPANESE_RECIPES } from "./japanese";
 import { KOREAN_RECIPES } from "./korean";
+import { TAIWANESE_RECIPES } from "./taiwanese";
 
 export const GLOBAL_RECIPES = [
   ...EAST_ASIA_RECIPES,
   ...JAPANESE_RECIPES,
   ...KOREAN_RECIPES,
+  ...TAIWANESE_RECIPES,
 ];
